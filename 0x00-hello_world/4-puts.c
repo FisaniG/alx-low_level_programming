c78561d708dcf90d378263687674f8f49fb09a2b
FisaniG <fisanig@gmail.com>
2022-09-09 07:53:53 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the size of various types  
- * Return:0 if excited properly, non-zero otherwise 
+ * main - print the size of various types
+ * Return:0 if excited properly, non-zero otherwise
  */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-    return (0);
-   }
+return (0);
+}
