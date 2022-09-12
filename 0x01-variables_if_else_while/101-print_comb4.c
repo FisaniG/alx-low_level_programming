b30f466c77622506d2142294f9743f9974af0a33
FisaniG <fisanig@gmail.com>
2022-09-12 05:22:32 -0700

    print combinations of three different numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,27 +11,26 @@ int a;
 int b;
 int c;
 
-for (a = 48; a < 58; a++)
+for (a = 48; a <= 57; a++)
 {
-for (b = 49; b < 58; b++)
+for (b = 49; b <= 57; b++)
 {
-for (c = 50; c < 58; c++)
+for (c = 50; c <= 57; c++)
+{
+if (c > b && b > a)
 {
 putchar(a);
-
 putchar(b);
-
 putchar(c);
 
-
-if (c > b && b > a)
-
 {
+
 if (a != 55 || b != 56)
 {
 putchar(44);
 
 putchar(32);
+}
 }
 }
 }
