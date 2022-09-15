372cbc46a435a27ed92cac97b63675d5ae760839
FisaniG <fisanig@gmail.com>
2022-09-14 20:45:02 -0700

    islower

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * print_last_digit(int) - print last digit
- * @a : interger
- * Return: 0.
+ * print_last_digit(int a) - print last digit of an interger.
+ * @a : interger number
+ * Return: interger.
  */
 
 int print_last_digit(int a)
 {
 int b = a % 10;
+if (b < 0)
+{
+b = b *  -1;
+_putchar(b + '0');
+}
 
 return (b);
 
