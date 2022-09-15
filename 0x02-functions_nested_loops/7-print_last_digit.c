c2a3e65f4701bad0ba529dd649209c788ea8e3c5
FisaniG <fisanig@gmail.com>
2022-09-14 20:11:51 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit(int) - print last digit
+ * @a : interger
+ * Return: 0.
+ */
+
+int print_last_digit(int a);
+{
+int a;
+int b = a % 10;
+
+return (b);
+
+}
