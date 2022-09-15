86b420cca4d2f68d35b37fa657f7c3c32298e80a
FisaniG <fisanig@gmail.com>
2022-09-15 07:11:24 -0700

    task 0-4

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if it a digit
+ *@c : number to be checked
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+return (c <= 48 && c >= 57);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiple two intergers
+ * @b : interger value
+ * @a : interger value
+ * Return: number
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
