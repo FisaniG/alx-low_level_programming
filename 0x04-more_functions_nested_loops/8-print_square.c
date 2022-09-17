431a6bbe4f6b5caf2a0ced216b3054268a827d44
FisaniG <fisanig@gmail.com>
2022-09-16 18:04:11 -0700

    print squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,16 @@
 void print_square(int size)
 {
 int a;
+int count = 0;
 
-while (size > 0)
+while (count < size)
 {
 for (a = 0; a < size; a++)
 {
 _putchar('#');
 }
 _putchar('\n');
-size--;
+count++;
 }
 if (size == 0)
 {
