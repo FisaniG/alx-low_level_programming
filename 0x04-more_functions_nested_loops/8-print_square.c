edba62d98edd8d76f4bb6dd5380a7e48df5fccb7
FisaniG <fisanig@gmail.com>
2022-09-16 17:23:38 -0700

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - prints square block of '#'
+ * @size : number of times '#' will be printed
+ * Return 0
+ */
+
+void print_square(int size)
+{
+int a;
+
+while (size > 0)
+{
+for (a = 0; a <= size; a++)
+{
+_putchar('#');
+}
+_putchar('\n');
+size--;
+}
+if (size == 0)
+{
+_putchar('\n');
+}
+if (size < 0)
+{
+_putchar('\n');
+}
+}
