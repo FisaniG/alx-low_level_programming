0be5454869d8bc9785f3d0e6e96f2ff82b96f201
FisaniG <fisanig@gmail.com>
2022-09-22 00:38:12 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
 
-for (i = 0; dest[i] != '\0'; ++i)
-{
 }
-for (j = 0; src[j] != '\0'; j++)
-{
-++i;
-dest[i] = src[j];
-}
-dest[i] = '\0';
+*dest = '\0';
 return (dest);
 }
