841b8119f7d2c9fa2c3b10db45a4106ff3900674
FisaniG <fisanig@gmail.com>
2022-09-23 01:17:45 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n; &&src[i] != '\0'; i++)
+char *temp = dest;
+while (n--)
 {
-dest[i] = src[i];
+(*dest++ = *src++);
 }
 
-dest[i] = '\0';
-return (dest);
+*dest = '\0';
+return (temp);
 }
