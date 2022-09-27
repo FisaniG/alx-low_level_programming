fc10f931b256ea8c7b6c0b4cdf07d5d2ad5d9e6e
FisaniG <fisanig@gmail.com>
2022-09-27 06:23:04 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,15 +12,16 @@ void print_diagsums(int *a, int size)
 int i;
 int j;
 int sum = 0;
+int *array = a;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
 if (i == j)
 {
-sum = sum + a[i][j];
+sum = sum + array[i][j];
 }
 }
 }
-return (sum);
+printf("%d", sum);
 }
