19799d54832b19d38fe5f1a4dff2e7fed1ee9639
FisaniG <fisanig@gmail.com>
2022-09-27 02:10:55 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,22 +4,22 @@
  * *_strpbrk - point to substring
  * @s: string
  * @accept: substring
- * Return': char string
+ * Return: char string
  */
 char *_strpbrk(char *s, char *accept)
 {
 int i;
 int j;
 char *scan;
-while ((i = *s++) != 0))
+while ((i = *s++) != 0)
 {
-for (scan = accept; j = (*scan++)!= 0)
+for (scan = accept; (j = *scan++) != 0;)
 {
 if (j == i)
 {
 return ((char *)s - 1);
 }
 }
-return (0);
 }
+return (0);
 }
