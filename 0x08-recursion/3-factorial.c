39812b6cb687bb074b69b44f54e3bdb17a1c01e4
FisaniG <fisanig@gmail.com>
2022-09-28 09:47:51 -0700

    main.h updates

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - check for factors
+ * @n: interger checked
+ * Return: 0
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (0);
+}
+return (n * factorial(n - 1));
+}
