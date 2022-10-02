aa03da58b45361a9211083a17132adbad8f14f6a
FisaniG <fisanig@gmail.com>
2022-10-01 22:46:24 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,21 +9,23 @@
 int _atoi(char *s)
 {
 int sign = 1;
+int i = 0;
 unsigned int num = 0;
-while (*s++)
+while (s[i] != '\0')
 {
-if (*s == '-')
+if (s[i] == '-')
 {
 sign *= -1;
 }
-else if (*s >= '0' && *s <= '9')
+else if (s[i] >= '0' && s[i] <= '9')
 {
-num = (num * 10) + (*s - '0');
+num = (num * 10) + (s[i] - '0');
 }
 else if (num > 0)
 {
 break;
 }
+i++;
 }
 
 num *= sign;
