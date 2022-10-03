6240ae51a28790ccfa95e5e95196fee0f8801db2
FisaniG <fisanig@gmail.com>
2022-10-03 10:15:34 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+ * _strdup - duplicate string
+ * @str: pointer to string
+ * Return: char string
+ **/
+char *_strdup(char *str)
+{
+char *p;
+int i = 0;
+char *dup;
+if (str == 0)
+{
+return (0);
+}
+while (str[i])
+{
+i++;
+}
+dup = malloc(i + 1);
+p = dup;
+while (*str)
+{
+*p++ = *str++;
+}
+*p = '\0';
+return (dup);
+free(dup);
+}
