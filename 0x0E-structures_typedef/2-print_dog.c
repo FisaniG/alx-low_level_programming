108016f848fa9a246659c4473f29b729913e5f89
FisaniG <fisanig@gmail.com>
2022-10-10 15:02:55 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,20 +10,31 @@
 void print_dog(struct dog *d)
 {
 if (d == 0)
-return;
-if (d != 0)
 {
+return;
+}
 if (d->name == 0)
+{
 printf("Name: (nul)\n");
+}
 else
+{
 printf("Name: %s\n", d->name);
+}
 if (d->age == 0)
+{
 printf("Age: (nil)\n");
+}
 else
+{
 printf("Age: %f\n", d->age);
+}
 if (d->owner == 0)
+{
 printf("Owner: (nil)\n");
+}
 else
+{
 printf("Owner: %s\n", d->owner);
 }
 }
