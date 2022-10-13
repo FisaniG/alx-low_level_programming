d0553303c3410968f8e431ef8fa811a2d352dc8d
FisaniG <fisanig@gmail.com>
2022-10-13 13:54:49 -0700

    debugging

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,10 +12,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list(ap);
 char *str;
-str = va_arg(ap, char *);
 va_start(ap, n);
 for (i = 0; i < n; i++)
 {
+str = va_arg(ap, char *);
 if
 (str == NULL)
 {
