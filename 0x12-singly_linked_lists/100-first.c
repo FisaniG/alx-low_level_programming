f4652d999a99622b324e9c36100d9b5a97df0e69
FisaniG <fisanig@gmail.com>
2022-10-23 06:26:36 -0700

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * printfirst - execure first
+ * Return: void
+ */
+void __attribute__((constructor)) printfirst()
+{
+printf("You're beat! but yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
