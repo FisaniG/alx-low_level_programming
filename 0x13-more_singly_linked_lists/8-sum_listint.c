ad433a232b0adede3aba4c30b232a6bf46f284e9
FisaniG <fisanig@gmail.com>
2022-10-25 21:34:15 -0700

    task 8 betty fix

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * sum_listint - Function
  *
@@ -10,14 +11,16 @@
  */
 int sum_listint(listint_t *head)
 {
-  int sum = 0;
+	int sum = 0;
 
-  if (head == NULL)
-    return (0);
-  while (head)
-    {
-      sum += head->n;
-      head = head->next;
-    }
-  return (sum);
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
