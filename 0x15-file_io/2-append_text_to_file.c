a99653d72e7dd812cee298244dbc53c64db9d635
FisaniG <fisanig@gmail.com>
2022-11-02 04:15:59 -0700

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,11 +25,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i]; i++)
 		{
-		}			;
+		}
 
 		rwr = write(fd, text_content, i);
 		if (rwr == -1)
+		{
+			close(fd);
 			return (-1);
+		}
 	}
 
 	close(fd);
