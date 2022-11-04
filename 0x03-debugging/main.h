f24b1a7bcd24abae59f7c8752144ce6365553aa5
FisaniG <fisanig@gmail.com>
2022-11-04 09:15:21 -0700

    largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,8 +6,10 @@
 
 
 
-pstitive_or_negative()
-print_remaining_days()
-int   largest_number(int a, int b, int c); 
+void postitive_or_negative(int i);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day , int year);
+int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
   
