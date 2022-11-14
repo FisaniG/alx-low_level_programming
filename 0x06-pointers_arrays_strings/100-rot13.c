80c56ba8106d9e0187550dc5558226b32f7565dd
FisaniG <fisanig@gmail.com>
2022-11-14 09:11:37 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,16 +7,16 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXWYZ";
-	char b[] = "nopqrstuvxwyzabcdefghijklmNOPQRSTUVXWYZABCDEFGHIJKLM";
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (a[j] == *(s + i))
+			if (a[j] == s[i])
 			{
-				*(s + i) = b[j];
+				s[i] = b[j];
 				break;
 			}
 		}
