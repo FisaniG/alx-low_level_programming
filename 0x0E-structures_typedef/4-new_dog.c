1bbc3deb9d98fe6267c2f86a18b0e9265cc981d1
FisaniG <fisanig@gmail.com>
2022-11-27 05:07:57 -0800

    4-new dog and 5-free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,28 +10,44 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *ptr;
-int j = 0, i = 0, k;
-while (name[i] != '\0')
-i++;
-while (owner[j] != '\0')
-j++;
-ptr = malloc(sizeof(dog_t));
-if (ptr == NULL)
-return (NULL);
-ptr->name = malloc(i + 1);
-if (ptr->name == NULL)
-free(ptr);
-return (NULL);
-for (k = 0; k <= i + 1; k++)
-ptr->name[k] = name[k];
-ptr->age = age;
-ptr->owner = malloc(j + 1);
-if (ptr->owner == NULL)
-free(ptr->name);
-free(ptr);
-return (NULL);
-for (k = 0; k <= j + 1; k++)
-ptr->owner[k] = owner[k];
-return (ptr);
+	dog_t *ptr;
+	char *cname, *cowner;
+	int len1, len2, i;
+
+	ptr = malloc(sizeof(dog_t));
+	if (!ptr)
+		return (NULL);
+	for (len1 = 0; name[len1]; len1++)
+		;
+	for (len2 = 0; owner[len2]; len2++)
+		;
+	len1++;
+	len2++;
+	cname = malloc(len1);
+	if (!cname)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	cowner = malloc(len2);
+	if (!cowner)
+	{
+		free(cname);
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		cname[i] = name[i];
+	}
+	for (i = 0; i < len2; i++)
+	{
+		cowner[i] = owner[i];
+	}
+
+	ptr->name = cname;
+	ptr->owner = cowner;
+	ptr->age = age;
+
+	return (ptr);
 }
