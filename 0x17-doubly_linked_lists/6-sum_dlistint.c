3a8a5a4e7b42d545a0288ddd43c48c0cec87fbc4
FisaniG <fisanig@gmail.com>
2022-12-08 12:27:36 -0800

    sum of node ns

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum all n's
+ * @head: head of list
+ * Return: 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
