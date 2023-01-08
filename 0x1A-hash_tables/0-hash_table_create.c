0252de3dfe265ad059778ae7f2f09d3a407abbae
FisaniG <fisanig@gmail.com>
2023-01-08 02:32:56 -0800

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable = malloc(sizeof(hash_table_t));
-	unsigned int i; /* to iterate the array */
+	unsigned long int i; /* to iterate the array */
 	/* check if the table was created */
 	if (!hashtable)
 		return (NULL);
