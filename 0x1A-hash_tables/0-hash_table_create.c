7e040c331c0f98b7ecd0ab32e97c4ecf9439d598
FisaniG <fisanig@gmail.com>
2023-01-08 02:55:45 -0800

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hashtable = malloc(sizeof(hash_table_t));
 	unsigned long int i; /* to iterate the array */
 	/* check if the table was created */
-	if (hashtable == NULL)
+	if (!hashtable)
 		return (NULL);
 	/* now, create the array to save the data */
 	hashtable->size = size;
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
-	if (hashtable->array == NULL)
+	if (!hashtable->array)
+	{
+		free(hashtable);
 		return (NULL);
+	}
 	/* finnaly, initialize the array whith all elements as null */
 	for (i = 0; i < size; i++)
 	hashtable->array[i] = 0;
